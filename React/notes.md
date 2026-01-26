b4f1c904cae34f2a092a6d65471cfc89879342d0
YashBarbole <yashbarbole7110@gmail.com>
2026-01-26 20:04:40 +0530

    js revised

--- React/notes.md
+++ React/notes.md
@@ -7,3 +7,8 @@ we do client side routing
 component takes prop as input 
 it  can be any component , number, string 
 
+we should use usenavigate inside browserrouter always
+
+react router dom introduced lazy loading
+
+we try to push down state 
--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,33 +1,42 @@
-import { BrowserRouter, Routes,Route } from "react-router-dom"
+import { BrowserRouter, Routes,Route, useNavigate } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import Landing from "./pages/Landing"
 
 function App() {
-  
-  
   return (
+    
     <div>
-      <div>
-        <button onClick={()=>{
-            window.location.href="/";
-        }}> Landing</button>
 
-        <button onClick={()=>{
-          window.location.href="/dashboard";
-        }}> Dashboard</button>
-
-
-      </div>
- 
     <BrowserRouter>
+
+    <Appbar></Appbar>
+
     <Routes>
       <Route path="/dashboard" element={<Dashboard/>}></Route>
       <Route path="/" element={<Landing/>}></Route>
     </Routes>
+
     </BrowserRouter>
 
        </div>
   )
+
+
+function Appbar(){
+  
+const navigate= useNavigate();
+
+return <div>
+       <div>  <button onClick={()=>{
+        navigate("/");
+        }}> Landing</button>
+
+        <button onClick={()=>{
+         navigate("/dashboard");
+        }}> Dashboard</button>
+        </div>
+      </div>
+  }
 }
 
 export default App
